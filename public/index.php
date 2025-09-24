3848d4f787f48debf81ddb203cceb1b371ae2b9b
Cyril Muchemi <cyrilmuchemi@gmail.com>
2025-09-24 17:20:45 +0300

    Create a working router

--- public/index.php
+++ public/index.php
@@ -1,11 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Custom MVC</title>
-</head>
-<body>
-    <h1>I want food</h1>
-</body>
-</html>
+<?php
+
+function show($stuff){
+    echo "<pre>";
+    print_r($stuff);
+    echo "</pre>";
+}
+
+function split_URL(){
+    $URL = isset($_GET["url"]) ? trim($_GET["url"], "/") : "home";
+    $URL = explode("/", $URL);
+    return $URL;
+}
+
+function load_controller(){
+    $URL = split_URL();
+    $file_name = "../app/controllers/".ucfirst($URL[0]).".php";
+
+    if(file_exists($file_name)){
+        require $file_name;
+    }else{
+        $file_name = "../app/controllers/_404.php";
+        require $file_name;
+    }
+}
+
+load_controller();
