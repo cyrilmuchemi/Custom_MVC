60e6719702ce17eec774a023e731453edcf108a4
Cyril Muchemi <cyrilmuchemi@gmail.com>
2025-09-25 11:12:10 +0300

    Add core files

--- public/index.php
+++ public/index.php
@@ -1,27 +1,7 @@
 <?php
 
-function show($stuff){
-    echo "<pre>";
-    print_r($stuff);
-    echo "</pre>";
-}
+session_start();
+require "../app/core/init.php";
 
-function split_URL(){
-    $URL = isset($_GET["url"]) ? trim($_GET["url"], "/") : "home";
-    $URL = explode("/", $URL);
-    return $URL;
-}
-
-function load_controller(){
-    $URL = split_URL();
-    $file_name = "../app/controllers/".ucfirst($URL[0]).".php";
-
-    if(file_exists($file_name)){
-        require $file_name;
-    }else{
-        $file_name = "../app/controllers/_404.php";
-        require $file_name;
-    }
-}
-
-load_controller();
+$app = new App;
+$app->load_controller();
