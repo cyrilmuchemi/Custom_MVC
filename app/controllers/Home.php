3a42dac7b488240fb282a8a3236c8cb9f24b61c2
Cyril Muchemi <cyrilmuchemi@gmail.com>
2025-09-30 16:30:34 +0300

    Modify the Model class

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -4,7 +4,7 @@ class Home extends Controller
 {
     public function index($a = '', $b = '', $c = ''){
         $model = new Model;
-        $result = $model->where(['id' => 1]);
+        $result = $model->delete(5);
         show($result);
         $this->view('home');
     }
--- app/core/Model.php
+++ app/core/Model.php
@@ -13,25 +13,54 @@ class Model
         $keys_not = array_keys($data_not);
         $query = "SELECT * FROM $this->table WHERE ";
         foreach ($keys as $key) {
-            $query .= $key . "= :" . $key . " && ";
+            $query .= $key . " = :" . $key . " && ";
         }
 
         foreach ($keys_not as $key) {
-            $query .= $key . "!= :" . $key . " && ";
+            $query .= $key . " != :" . $key . " && ";
         }
 
         $query = trim($query, " && ");
         $query .= " limit $this->limit offset $this->offset";
 
-        echo $query;
+        $data = array_merge($data, $data_not);
+
+        return $this->query($query, $data);
     }
 
     public function first($data, $data_not = []){
-        
+        $keys = array_keys($data);
+        $keys_not = array_keys($data_not);
+        $query = "SELECT * FROM $this->table WHERE ";
+        foreach ($keys as $key) {
+            $query .= $key . " = :" . $key . " && ";
+        }
+
+        foreach ($keys_not as $key) {
+            $query .= $key . " != :" . $key . " && ";
+        }
+
+        $query = trim($query, " && ");
+        $query .= " limit $this->limit offset $this->offset";
+
+        $data = array_merge($data, $data_not);
+
+        $result = $this->query($query, $data);
+
+        if($result){
+            return $result[0];
+        }
+
+        return false;
     }
 
     public function insert($data){
+        $keys = array_keys($data);
+        $query = "INSERT into $this->table (".implode(",", $keys).") values (:".implode(",:", $keys).")";
 
+        $this->query($query, $data);
+
+        return false;
     }
 
     public function update($id, $data, $id_column = 'id'){
@@ -39,6 +68,12 @@ class Model
     }
 
     public function delete($id, $id_column = 'id'){
-        
+
+        $data[$id_column] = $id;
+        $query = "DELETE FROM $this->table WHERE $id_column = :$id_column";
+
+        $this->query($query, $data);
+
+        return false;
     }
 }
